2dd5384d6feb8ed84c267fd6104702153e6c5ed6
Ivan Kotov <polyzay@gmail.com>
2024-06-01 17:01:29 +0200

    cvtable was removed

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,6 @@
   import Card from './lib/Card.svelte';
   import Info from './lib/Info.svelte';
   import SendCv from './lib/SendCv.svelte';
-  import CvTable from './lib/CvTable.svelte';
 
   function scrollToSection(event) {
     event.preventDefault();
@@ -59,10 +58,6 @@
         <div class="sendCv">
           <SendCv />
         </div>
-      </div>
-      <div>
-        <h2>table</h2>
-        <CvTable />
       </div>
       <div id="contacts" class="contacts">
         <h2>Контакты</h2>
