d508404a6dfa2a9e2abb98c7e27b8d71a32ee8b0
Ivan Kotov <polyzay@gmail.com>
2024-06-16 13:25:47 +0200

    features pics

--- src/lib/Features.svelte
+++ src/lib/Features.svelte
@@ -1,16 +1,19 @@
 <script>
-	import logo from '../assets/svelte.svg';
-const features = [
-	{picPath: '', header: 'для опытных и начинающих', paragraph: 'Наша база вакансий включает предложения для соискателей с любым уровнем опыта, от новичков до экспертов'},
-	{picPath: '', header: 'удаленная и локальная работа', paragraph: 'Мы предлагаем широкий выбор вакансий как для удаленной работы, так и для работы на месте'},
-	{picPath: '', header: 'оперативность и качество', paragraph: 'Благодаря нашей оперативности вы можете быть уверены в своевременном решении кадровых вопросов'},
+    import success from '../assets/success.svg';
+    import team from '../assets/team.svg';
+    import renovation from '../assets/renovation.svg';
+
+		const features = [
+	{picPath: team, header: 'для опытных и начинающих', paragraph: 'Наша база вакансий включает предложения для соискателей с любым уровнем опыта, от новичков до экспертов'},
+	{picPath: renovation, header: 'удаленная и локальная работа', paragraph: 'Мы предлагаем широкий выбор вакансий как для удаленной работы, так и для работы на месте'},
+	{picPath: success, header: 'оперативность и качество', paragraph: 'Благодаря нашей оперативности вы можете быть уверены в своевременном решении кадровых вопросов'},
 ]
 </script>
 <div class="features">
     {#each features as feature}
         <div class="feature">
             <div>
-                <img src={logo} alt="logo" class="company-logo">
+                <img src={feature.picPath} alt="logo" class="company-logo">
             </div>
             <div class="feature-content">
                 <h5>{feature.header}</h5>
@@ -38,7 +41,8 @@ const features = [
 		justify-content: space-between;
 	}
 	.company-logo {
-		max-width: 100px;
+		width: 100px;
+      height: 100px;
 		margin-bottom: 10px;
 	}
 	.feature-content {
