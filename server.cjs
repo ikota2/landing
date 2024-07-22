b6bb777f47b48f460053ef2ee44aaa39232eba2f
Ivan Kotov <polyzay@gmail.com>
2024-07-22 17:31:00 +0200

    valid experiment?

--- server.cjs
+++ server.cjs
@@ -79,8 +79,11 @@ app.post('/api/login', async (req, res) => {
 		// 	return res.status(401).send('Invalid username or password (jwt case)' + ' ' + `password: ${password} passwordHash: ${user.passwordHash}`);
 		// }
 
-		const token = jwt.sign({ userId: user._id }, jwtSecret, { expiresIn: '1h' });
-		res.json({ token });
+		const dbPassword = await collectionUsers.findOne({ password });
+		if (dbPassword === password) {
+			const token = jwt.sign({ userId: user._id }, jwtSecret, { expiresIn: '1h' });
+			res.json({ token }).status(200);
+		}
 	} catch (err) {
 		console.error('Error during login:', err);
 		res.status(500).send('Error during login');
